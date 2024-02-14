f65aaae558aee49afc3ba7081db1069623f2ab09
Jorge <141792594+JVPresti@users.noreply.github.com>
2024-02-13 18:47:19 -0800

    Create Persona.h

---
+++ ProyectoPrueba/Persona.h
@@ -0,0 +1,34 @@
+#pragma once
+class Persona {
+	//Los miembros de la clase pueden ser privados o publicos, por default son privados, pero se puede cambiar al tipo que nos convenga
+	//Existen 3 tipos de proteccion:
+	//Public: Los miembros son accesibles desde cualquier parte del programa
+	//Private: Los miembros son accesibles solo desde la misma clase
+	//Protected: Los miembros son accesibles desde la misma clase y desde las clases hijas
+	//Para poner el tipo de proteccion, es utilizada la palabra clave seguida de dos puntos antes de la declaracion de los miembros
+
+protected:
+	int edad;
+	//char name[30];
+	char* name;
+
+	void reir();
+	void comer();
+	
+public:
+	//Los objetos pueden tener constructores, que son funciones que se ejecutan automaticamente cuando el objeto es creado
+	//El constructor es una funcion que tiene el mismo nombre que la clase, y no tiene tipo de retorno
+	//Los constructores pueden tener argumentos, pero no pueden ser llamados directamente
+	Persona(const char* elName, int laEdad);
+	Persona();
+	//En c++ podemos usar el mismo nombre para diferentes funciones, siempre y cuando los parametros sean diferentes
+	//Ciamdp implementamos funciones de este tipo, se le llama sobrecarga de funciones
+
+	//Los objetos pueden tener un destructor, El destructor es una funcion que se ejecuta automaticamente cuando el objeto es eliminado
+	//El destructor es una funcion que tiene el mismo nombre que la clase, pero precedido por el simbolo ~
+	~Persona();
+
+	void saludar();
+	void hacerReir(int motivo);
+	void liberar();
+};
