f1c851bad3d0f088c35fff3dd6ffff4dfa9eadb5
Jorge <141792594+JVPresti@users.noreply.github.com>
2024-02-16 09:17:14 -0800

    Update Persona.cpp

--- ProyectoPrueba/Persona.cpp
+++ ProyectoPrueba/Persona.cpp
@@ -5,42 +5,23 @@
 using std::cout;
 using std::endl;
 
+//Funcion para reir
 void Persona::reir() {
 	cout << "Jajaja..." << endl;
 }
 
+//Funcion para comer
 void Persona::comer() {
 	cout << "nom, nom, nom" << endl;
 }
 
-Persona::Persona(){
-	//Los constructores se utilizan para inicializar atributos y reservr recursos que pudiera utilizar el objeto
-	edad = 20;
-	name = (char*) malloc(7); 
-	strncpy(name, "Sosimo", 6); // Copia el contenido de la cadena de texto en el segundo argumento al primer argumento
-	name[6] = 0;
-	
-}
-
-Persona::Persona(const char* elName, int laEdad){
-	edad= laEdad;
-	name=(char* ) malloc(strlen(elName)+1);
-	strncpy(name, elName, strlen(elName));
-	name[strlen(elName)] = 0;
-}
-
-Persona::~Persona(){
-	if (name != nullptr) {
-		cout << "Adios, " << name << endl;
-		free(name);
-	}
-}
-
+//Funcion que imprime un saludo
 void Persona::saludar(){
 	cout << "Hola, soy " << name;
 	cout << " y tengo " << edad << endl;
 }
 
+//Funcion que decide si se rie o no
 void Persona::hacerReir(int motivo){
 	if ((motivo % 3) == 0) {
 		reir();
@@ -50,6 +31,34 @@ void Persona::hacerReir(int motivo){
 	}
 }
 
+//Constructor que lo hace automatico
+Persona::Persona(){
+	//Los constructores se utilizan para inicializar atributos y reservr recursos que pudiera utilizar el objeto
+	edad = 20;
+	name = (char*) malloc(7); 
+	strncpy(name, "Sosimo", 6); // Copia el contenido de la cadena de texto en el segundo argumento al primer argumento
+	name[6] = 0;
+	
+}
+
+//Constructor para crear un objeto con nombre y edad especificos
+Persona::Persona(const char* elName, int laEdad){
+	edad= laEdad;
+	name=(char* ) malloc(strlen(elName)+1);
+	strncpy(name, elName, strlen(elName));
+	name[strlen(elName)] = 0;
+}
+
+//Elimina el objeto de la memoria
+Persona::~Persona(){
+	if (name != nullptr) {
+		cout << "Adios, " << name << endl;
+		free(name);
+	}
+}
+
+
+//Funcion que libera la memoria
 void Persona::liberar() {
 	free(name);
 	name = nullptr;
