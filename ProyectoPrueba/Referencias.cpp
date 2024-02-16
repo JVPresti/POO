f53c9686be3dc6cb98822f2b644dd9b5cd5a00f6
Jorge <141792594+JVPresti@users.noreply.github.com>
2024-02-16 09:57:00 -0800

    Update Referencias.cpp

--- ProyectoPrueba/Referencias.cpp
+++ ProyectoPrueba/Referencias.cpp
@@ -4,17 +4,31 @@
 using std::cout;
 using std::endl;
 
-void usarPersona(Persona *persona) {
+const double PI= 3.1416;
+
+void usarPersona(Persona &persona) {
     cout  << "----Utilizando persona----" << endl;
-    persona->saludar();
-    persona->hacerReir(rand());
+    persona.saludar();
+    persona.hacerReir(rand());
     cout << "---Persona utilizada---" << endl;
 
 }
 
+// Cuando se recibe un paramatreo por referencia, existe la posibilidad de que la funcion modifique el parametro afectando a la variable que utilizamos 
+// para llamar a la funcion.
+void usarPersona2(const Persona& persona) {
+	cout << "----Usando otra vez a Persona: "<< persona.getName() << "----" << endl;
+    persona.saludar();
+    //No se pueden invocar metodos que no sean const si recibimos una variable const
+    //persona.setName("Ifigenia");
+    //persona.setEdad(-20);
+    persona.hacerReir(rand());
 
-int main()
-{
+    cout << "--------------------------------" << endl;
+}
+
+
+int main() {
     srand(time(NULL));
 
     Persona persona1("Sempronio", 20);
@@ -24,8 +38,11 @@ int main()
     //persona3 = &persona1;
 
     persona1.saludar();
-    usarPersona(&persona2);
-    usarPersona(persona3);
+    usarPersona(persona2);
+    usarPersona(*persona3);
+
+    usarPersona2(persona1);
+    persona1.saludar();
 
     cout<< "Adios..." << endl;
 
