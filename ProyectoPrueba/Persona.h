7d6f28fa1a9a235bf4be35943a6df475a267caaa
Jorge <141792594+JVPresti@users.noreply.github.com>
2024-02-16 09:16:57 -0800

    Update Persona.h

--- ProyectoPrueba/Persona.h
+++ ProyectoPrueba/Persona.h
@@ -10,25 +10,31 @@ class Persona {
 protected:
 	int edad;
 	//char name[30];
-	char* name;
+	char* name; //Con memoria dinamica
 
 	void reir();
 	void comer();
 	
 public:
+	void saludar();
+	void hacerReir(int motivo);
+	void liberar();
+	
 	//Los objetos pueden tener constructores, que son funciones que se ejecutan automaticamente cuando el objeto es creado
 	//El constructor es una funcion que tiene el mismo nombre que la clase, y no tiene tipo de retorno
 	//Los constructores pueden tener argumentos, pero no pueden ser llamados directamente
-	Persona(const char* elName, int laEdad);
-	Persona();
+
+	//Constructores
+	Persona(const char* elName, int laEdad); //Asigna nombre y edad por parametro
+	Persona(); //Asigna nombre y edad por default
+
+	//Destructor
+	~Persona();
+
 	//En c++ podemos usar el mismo nombre para diferentes funciones, siempre y cuando los parametros sean diferentes
 	//Ciamdp implementamos funciones de este tipo, se le llama sobrecarga de funciones
 
 	//Los objetos pueden tener un destructor, El destructor es una funcion que se ejecuta automaticamente cuando el objeto es eliminado
 	//El destructor es una funcion que tiene el mismo nombre que la clase, pero precedido por el simbolo ~
-	~Persona();
 
-	void saludar();
-	void hacerReir(int motivo);
-	void liberar();
 };
