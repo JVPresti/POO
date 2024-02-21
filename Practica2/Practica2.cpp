44bbf0f72ae022e05dc4dbf3071a5de683f7ae5a
Jorge <141792594+JVPresti@users.noreply.github.com>
2024-02-21 10:42:26 -0800

    Update Practica2.cpp

--- Practica2/Practica2.cpp
+++ Practica2/Practica2.cpp
@@ -8,23 +8,29 @@ using std::endl;
 
 int main()
 {
-    CuentaDeCheques persona1("Juan", 1234, "2 de agosto");
-    CuentaDeCheques persona2("Pedro", 5678, "3 de agosto");
+    //srand(time(0));
+    //CuentaDeCheques persona1("Juan", 1234, "2 de agosto");
+    //CuentaDeCheques persona2("Pedro", 5678, "3 de agosto");
+    CuentaDeCheques persona1;
+    CuentaDeCheques persona2;
 
-    cout << "Depositar 1000 a la cuenta de Pedro" << endl;
     persona1.depositar(1000);
+    persona1.preguntarSaldo(&persona1);
 
-    cout << "Depositar 500 a la cuenta de Juan" << endl;
+    cout << "Depositar 500 a la cuenta de Pedro" << endl;
     persona2.depositar(500);
 
-    cout << "El total en la cuenta de Juan es de " << persona2.saldo << endl;
     cout << "Retirar 200 de la cuenta de Juan" << endl;
-    persona1.retirar(200); //! FALTA QUE ACTUALCE EL SALDO, NO LO REGRESA, CREO QUE TENGO QUE USAR PUNTEROS
-    cout << "El total en la cuenta de Juan es de " << persona2.saldo << endl;
+    persona1.retirar(200, &persona1); //! FALTA QUE ACTUALCE EL SALDO, NO LO REGRESA, CREO QUE TENGO QUE USAR PUNTEROS
+    //persona1.preguntarSaldo(&persona1);
 
     cout << "Transferir 100 de la cuenta de Juan a la cuenta de Pedro" << endl;
     persona1.transferir(100, &persona2);
-    cout << "El total en la cuenta de Juan es de " << persona2.saldo << endl;
     
+    persona2.preguntarSaldo(&persona2);
+    persona1.preguntarSaldo(&persona1);
+
     return 0;
 }
+
+//SOLO FALTA QUE ASIGNE EL NOMBRE AUTOMATICO, Y HACER UN MENU PARA HACER CUALQUIER COSA
