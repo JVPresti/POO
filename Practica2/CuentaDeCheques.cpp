176280b26b11322da69ff106897c85a0518a9456
Jorge <141792594+JVPresti@users.noreply.github.com>
2024-02-21 10:42:50 -0800

    Update CuentaDeCheques.cpp

--- Practica2/CuentaDeCheques.cpp
+++ Practica2/CuentaDeCheques.cpp
@@ -15,7 +15,7 @@ CuentaDeCheques::~CuentaDeCheques()
 	//free(fechaUltimaTransaccion); // ESTO ME CAUSA ERROR YA NO PUEDO MAS CON MI FOKIN VIDA
 }
 
-CuentaDeCheques::CuentaDeCheques(const char *name, int number, const char *fechax)
+CuentaDeCheques::CuentaDeCheques(const char* name, int number, const char* fechax)
 {
 	nombreCliente = new char[strlen(name) + 1];
 	strncpy_s(nombreCliente, strlen(name) + 1, name, _TRUNCATE);
@@ -29,6 +29,27 @@ CuentaDeCheques::CuentaDeCheques(const char *name, int number, const char *fecha
 	cout << "Cuenta de cheques creada" << endl;
 }
 
+CuentaDeCheques::CuentaDeCheques(){
+	//srand(time(NULL));
+	char nombres[][10] = {"Juan", "Pedro", "Maria", "Jose", "Luis", "Ana", "Sofia", "Carlos", "Fernando", "Ricardo"};
+	int random = rand() % 10;
+	nombreCliente = (char*)malloc(strlen(nombres[random]) + 1);
+	strncpy_s(nombreCliente, strlen(nombres[random]) + 1, nombres[random], _TRUNCATE); //! NO SE PORQUE NO ME DEJA COPIAR EL NOMBRE, MIRA EL DOCUMENTO ANTIGUO
+	numeroCuenta = rand() % 9999 + 1000;
+	saldo = 0;
+
+	//nombreCliente= (char*)malloc(8);
+	//strncpy(nombreCliente, "Ricardo", 7);
+	//nombreCliente[7] = '\0';
+
+	fechaUltimaTransaccion = new char[20];
+	time_t tm = time(0);
+	string fecha = ctime(&tm);
+	fecha.copy(fechaUltimaTransaccion, 20);
+	cout << "Cuenta de cheques creada" << endl;
+
+}
+
 double CuentaDeCheques::depositar(double cantidad)
 {
 	saldo += cantidad;
@@ -39,31 +60,31 @@ double CuentaDeCheques::depositar(double cantidad)
 	return saldo;
 }
 
-double CuentaDeCheques::retirar(double cantidad)
+double CuentaDeCheques::retirar(double cantidad, CuentaDeCheques* persona)
 {
 	try
 	{
-		if (cantidad <= saldo)
+		if (cantidad <= persona->saldo)
 		{
-			saldo -= cantidad;
+			persona->saldo -= cantidad;
 			time_t tm = time(0);
 			string fecha = ctime(&tm);
 			fecha.copy(fechaUltimaTransaccion, 50);
-			return ;
+			return persona->saldo;
 		}
 		else
 		{
 			throw "Saldo insuficiente";
 		}
 	}
-	catch (const char *msg)
+	catch (const char* msg)
 	{
 		cerr << "Error: " << msg << endl;
 		return -1;
 	}
 }
 
-double CuentaDeCheques::transferir(double cantidad, CuentaDeCheques *cuentaDestino)
+double CuentaDeCheques::transferir(double cantidad, CuentaDeCheques* cuentaDestino)
 {
 	if (cantidad <= saldo)
 	{
@@ -76,4 +97,11 @@ double CuentaDeCheques::transferir(double cantidad, CuentaDeCheques *cuentaDesti
 	}
 	else
 		return -1;
+}
+
+void CuentaDeCheques::preguntarSaldo(CuentaDeCheques* persona)
+{
+	cout << "El nombre del cliente es: " << persona->nombreCliente << endl;
+	cout << "El total en la cuenta es de " << persona->saldo << endl;
+	cout << "La fecha de la ultima transaccion fue: " << persona->fechaUltimaTransaccion << endl;
 }
