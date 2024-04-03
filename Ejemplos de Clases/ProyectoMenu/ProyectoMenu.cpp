16d3c17a5b385674a55ba4637af3b1d83b7ad967
Jorge <141792594+JVPresti@users.noreply.github.com>
2024-04-03 09:58:21 -0700

    Update ProyectoMenu.cpp

--- Ejemplos de Clases/ProyectoMenu/ProyectoMenu.cpp
+++ Ejemplos de Clases/ProyectoMenu/ProyectoMenu.cpp
@@ -1,9 +1,45 @@
 #include <iostream>
+#include "Menu.h"
+#include <conio.h>
 
 using std::cout;
 using std::endl;
 
-int main()
-{
-    
+void listado() {
+    cout << "Archivos en el folder: " << endl;
+    system("dir");
+    _getch();
+}
+
+void configuracionIp() {
+    cout << "Configuracion de IP" << endl;
+    system("ipconfig");
+    _getch();
+} 
+
+void conexionesActivas() {
+	cout << "Conexiones activas" << endl;
+	system("netstat");
+	_getch();
+}
+
+void configuracion() {
+    cout << "Pendiente..." << endl;
+    _getch();
+}
+
+int main(){
+    Menu elMenu("Menu de prueba");
+    //Agregamos opciones
+    elMenu.agregar({ '1', "Ver listado de archivos", listado });
+    elMenu.agregar({ '2',"Configuracion de IP", configuracionIp });
+    //elMenu.agregar({ '3', "Conexiones activas", conexionesActivas });
+	elMenu.agregar({ '3', "Configuracion", configuracion });
+    elMenu.agregar({ '4', "Salir", []() {cout << "Saliendo..." << endl; } });
+    do {
+        system("cls");
+        elMenu.desplegar();
+    }while(elMenu.seleccionar() != '4');
+
+        
 }
