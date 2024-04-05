b5221158124ad858c90f5f1521928a9c2d4993fd
Jorge <141792594+JVPresti@users.noreply.github.com>
2024-04-05 09:58:29 -0700

    Update ProyectoUniversidad.cpp

--- Ejemplos de Clases/ProyectoUniversidad/ProyectoUniversidad.cpp
+++ Ejemplos de Clases/ProyectoUniversidad/ProyectoUniversidad.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "Materia.h"
+#include <iomanip>
 
 using std::cout;
 using std::endl;
@@ -9,6 +10,11 @@ int main()
     Materia m1(12345, "Programacion y metodos numericos", 7, true, Materia::Etapa::BASICA);
     Materia m2(23456, "Estadistica Avanzada", 6, true, Materia::Etapa::BASICA);
     
+    cout << " Clave ";
+    cout << std::setw(50) << std::left << " Nombre";
+    cout << "Creditos tipo Etapa" << endl;
+
+
     cout << m1.toString() << endl;
     cout << m2.toString() << endl;
 }
